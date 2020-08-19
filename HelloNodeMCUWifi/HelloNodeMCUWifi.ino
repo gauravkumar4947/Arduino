b7f138a002ac7bf3bb7ede6d0df12a94f6b00687
gauravkumar4947 <61632883+gauravkumar4947@users.noreply.github.com>
2020-08-19 13:54:52 -0400

    Update HelloNodeMCUWifi.ino

--- HelloNodeMCUWifi/HelloNodeMCUWifi.ino
+++ HelloNodeMCUWifi/HelloNodeMCUWifi.ino
@@ -1,40 +1,88 @@
 #include <ESP8266WiFi.h>
 
-const char* ssid="Your SSID";
-const char* password = "Your Password";
+const char* ssid = "Fairway 2.4";
+const char* password = "*********";
+const char* host = "maker.ifttt.com";
+int Sensor = 14;
+int Pirled = 12;
+int Speaker= 13;
+int c = 262;
+int d = 294;
+int e = 330;
+int f = 349;
+int g = 392;
+int a = 440;
+int b = 494;
+int bf = 466;
+int C = 523;
+int frequency[56] = {
+  c, d, f, d, a, a, g, c, d, f, d, g, g, f, e, d, c, d, f, d, f, g, e, d, c, c, g, f,
+  c, d, f, d, a, a, g, c, d, f, d, C, e, f, e, d, c, d, f, d, f, g, e, d, c, c, g, f};
+int duration[56] = {
+  120, 120, 120, 120, 360, 360, 720, 120, 120, 120, 120, 360, 360, 480, 120, 240, 120, 120, 120, 120,
+  480, 240, 360, 120, 480, 240, 480, 960,
+  120, 120, 120, 120, 360, 360, 720, 120, 120, 120, 120, 360, 360, 480, 120, 240, 120, 120, 120, 120,
+  480, 240, 360, 120, 480, 240, 480, 960};
 
-int ledPin = 13;
 
 void setup() {
-  
-  pinMode(ledPin,OUTPUT);
-  digitalWrite(ledPin,LOW);
+    Serial.begin(115200);
 
-  Serial.begin(115200);
-  Serial.println();
-  Serial.print("Wifi connecting to ");
-  Serial.println( ssid );
+    Serial.println("Email from Node Mcu");
+    delay(100);
 
-  WiFi.begin(ssid,password);
-
-  Serial.println();
-  Serial.print("Connecting");
-
-  while( WiFi.status() != WL_CONNECTED ){
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    
+    WiFi.begin(ssid, password);
+    
+    while (WiFi.status() != WL_CONNECTED) {
       delay(500);
-      Serial.print(".");        
-  }
+      Serial.print(".");
+    }
+  
+    Serial.println("");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());  
 
-  digitalWrite( ledPin , HIGH);
-  Serial.println();
+    }
 
-  Serial.println("Wifi Connected Success!");
-  Serial.print("NodeMCU IP Address : ");
-  Serial.println(WiFi.localIP() );
+void loop() {  
+      
+  long state= digitalRead(Sensor);
+  if (state == HIGH)
+  { Serial.println("Motion detected");
+    digitalWrite( Pirled , HIGH);
+    for(int i=0; i<56; i++)
+    { 
+    tone(Speaker, frequency[i]); 
+    delay(duration[i]);
+    }
+    
+   WiFiClient client; 
+           const int httpPort = 80;  
+            if (!client.connect(host, httpPort)) 
+      {  
+                  Serial.println("connection failed");  
+            return;} 
+          
+    
+          
+                    String url = "/trigger/Test/with/key/d3yQCmCxTGg8cIohL2ofvkClJxH8YVJqo4shwDnXO3R"; 
 
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+          
+                    Serial.print("Requesting URL: ");
+                    Serial.println(url);
+                 
+                     client.print(String("GET ") + url + " HTTP/1.1\r\n" + 
+                                    "Host: " + host + "\r\n" +   
+                                           "Connection: close\r\n\r\n");    
+                      }
+          
+  else
+   Serial.println("NO motion detected");
+    digitalWrite( Pirled , LOW);
+  delay(1000);
+          
+      
 }
